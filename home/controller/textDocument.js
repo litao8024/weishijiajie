3fc2029de42f97c3becd9c5b64080441970d9ac1
litao8024 <18335929560@163.com>
2017-08-16 11:12:42 +0800

    测试

--- home/controller/textDocument.js
+++ home/controller/textDocument.js
@@ -115,7 +115,7 @@ function compressImage(src,dstname){
             alert("Compress error!"); 
         }); 
 }	
-// 产生一个随机数 
+// 产生一个随机数  
 function getUid() { 
     return Math.floor(Math.random() * 100000000 + 10000000).toString(); 
 }
